71b947e5f16501837ff5300ebc9703df07fb5222
Enrico N. Capone <ecapone@wbpstudio.com>
2014-07-31 18:55:02 -0700

    Added quit option to menu.

--- Tanks/Menu.java
+++ Tanks/Menu.java
@@ -3,29 +3,56 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.Image;
 import java.awt.Color;
-
+import java.awt.Font;
 public class Menu implements GameState {
     
     private Application app;
     private Image backgroundImage;
+    private int cursor = 0;
 
     public Menu(Application app) {
         this.app = app;
-        backgroundImage = Utils.loadImage("Tanks.png");
+        backgroundImage = Utils.loadImage("Menu.png");
     }
 
     public void draw(Graphics g){
         Utils.drawSprite(g, backgroundImage, 0, 0, 320, 240, 0, 0);
-        g.setColor(new Color(0x6B,0x8E,0x23));
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Castellar", Font.PLAIN, 20));
+        if(cursor == 0) {
+            g.setColor(Color.WHITE);
+        }
         g.drawString("Start!", 140, 100);
+        g.setColor(Color.BLACK);
+        if(cursor == 1) {
+            g.setColor(Color.WHITE);
+        }
+        g.drawString("Quit!", 140, 130);
     }
 
     public void update(Set<Integer> keysPressed, Set<Integer> previousKeysPressed){
+        if(Utils.keyJustPressed(KeyEvent.VK_DOWN, keysPressed, previousKeysPressed)) {
+            cursor = cursor + 1;
+            if(cursor > 1) {
+                cursor = 0;
+            }
+        }
+        if(Utils.keyJustPressed(KeyEvent.VK_UP, keysPressed, previousKeysPressed)) {
+            cursor = cursor - 1;
+            if(cursor < 0) {
+                cursor = 1;
+            }
+        }
         if(keysPressed.contains(KeyEvent.VK_ESCAPE)) {
             app.setRunning(false);
         }
         if(keysPressed.contains(KeyEvent.VK_ENTER)){
-            app.switchState(TanksSession.class);
+            if(cursor == 0) {
+                app.switchState(TanksSession.class);
+            }
+            if(cursor == 1) {
+                System.exit(0);
+            }
         }
     }
 }
